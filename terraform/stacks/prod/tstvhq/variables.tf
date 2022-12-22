674aa4ecff089a491a1a8326f632f84d5ab53099
Egor M <egor.miadzvedzeu@toptal.com>
2022-12-22 14:42:34 -0500

    Update production

--- terraform/stacks/prod/tstvhq/variables.tf
+++ terraform/stacks/prod/tstvhq/variables.tf
@@ -2,12 +2,17 @@
 # Production environment for tstvhq.myshopify.com
 #
 
+variable "bus_name" {
+  description = "Shopify event bus name"
+  type        = string
+}
+
 variable "cognito_user_pool_id" {
   description = "AWS Cognito user pool ID"
   type        = string
 }
 
-variable "shopify_shop_url" {
+variable "shopify_shop_domain" {
   description = "Shopify Shop URL"
   type        = string
 }
